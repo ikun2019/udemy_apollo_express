7e2f451001636d0cabad92baa7bc11eb31cbbaa8
ikun <0215.miyatto@gmail.com>
2024-04-10 05:54:55 +0900

    clientの作成

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:20.10.0-alpine
-WORKDIR /app
-
-# COPY package.json ./
-# RUN npm install
-
-# COPY . .
-# CMD npm run start
---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <main className='section'>
+      <Routes>
+        <Route index path='/' />
+      </Routes>
+    </main>
+  );
+}
+
+export default App;
